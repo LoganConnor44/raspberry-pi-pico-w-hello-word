4f94240b9a2af001b9810944b0c1e4864eedad21
Logan Connor <loganconnor44@gmail.com>
2023-12-09 21:52:13 -0500

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,33 @@
+import sys
+import network
+import time
+import urequests
+from machine import Pin
+from credentials import WIFI_NAME, WIFI_PASSWORD
+
+def lightShow() :
+    print("Internet Connection Made")
+    print("Attempting To Request Public API Now")
+    response = urequests.get("http://api.open-notify.org/astros.json").json()
+    if response["message"] != "success" :
+        print("Exiting : Improper Response Given")
+        sys.exit()
+
+    pin = Pin("LED", Pin.OUT)
+    for astronaut in response["people"]:
+        pin.toggle()
+        pin.off()
+        print(astronaut['name'])
+        time.sleep(2)
+
+    time.sleep(15)
+
+
+# wlan = network.WLAN(network.STA_IF)
+# wlan.active(True)
+# wlan.connect(WIFI_NAME, WIFI_PASSWORD)
+
+# if not wlan.isconnected() :
+#     print("Exiting : No Internet Connection")
+#     sys.exit()
+
